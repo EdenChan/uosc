9825dd6999f169a4c49706c879814eb7746b2ff0
edennow <wlws2012@163.com>
2023-04-27 20:56:05 +0800

    commit

--- scripts/uosc.lua
+++ scripts/uosc.lua
@@ -503,15 +503,14 @@ function update_margins()
 	if display.height == 0 then return end
 
 	local function causes_margin(element)
-		--return element and element.enabled and (element:is_persistent() or element.min_visibility > 0.5)
-		return element and element.enabled
+		return element and element.enabled and (element:is_persistent() or element.min_visibility > 0.5)
 	end
 	local timeline, top_bar, controls, volume = Elements.timeline, Elements.top_bar, Elements.controls, Elements.volume
 	-- margins are normalized to window size
 	local left, right, top, bottom = 0, 0, 0, 0
 
-	if causes_margin(controls) then bottom = (display.height - controls.ay) / display.height
-	elseif causes_margin(timeline) then bottom = (display.height - timeline.ay) / display.height end
+	if controls and controls.enabled then bottom = (display.height - controls.ay) / display.height
+	elseif timeline and timeline.enabled then bottom = (display.height - timeline.ay) / display.height end
 
 	if causes_margin(top_bar) then top = top_bar.title_by / display.height end
 
